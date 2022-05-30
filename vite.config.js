14ccadaeae04e491a82c2ab9a3998d86f65c8210
Emmanuel <eumana@inboundcycle.com>
2022-05-30 07:19:10 +0200

    build folder

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: './docs'
-  }
+  plugins: [vue()]
+  // build: {
+  //   outDir: './docs'
+  // }
 })
